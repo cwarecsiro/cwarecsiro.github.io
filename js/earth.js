2b5f66f626d8f690c5dd3a0644c9eebba03e8e06
Ware, Chris (L&W, Black Mountain) <Chris.Ware@csiro.au>
2018-02-15 13:48:53 +1100

    added parc with ocean and antatctic

--- js/earth.js
+++ js/earth.js
@@ -21,8 +21,8 @@
 	var camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
 	camera.position.z = 1.5;
 
-	var renderer = new THREE.WebGLRenderer({ alpha: true });
-	//var renderer = new THREE.WebGLRenderer()
+	//var renderer = new THREE.WebGLRenderer({ alpha: true });
+	var renderer = new THREE.WebGLRenderer()
 	renderer.setSize(width, height);
 
 	scene.add(new THREE.AmbientLight(0x333333));
@@ -61,9 +61,9 @@
 			new THREE.SphereGeometry(radius, segments, segments),
 			new THREE.MeshPhongMaterial({
 				//map:         THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg'),
-				map:         THREE.ImageUtils.loadTexture('images/parC_2016_meanC.png'),
+				map:         THREE.ImageUtils.loadTexture('images/parC_2016_Global.png'),
 				// new line below
-				alphaTest: 0.1,
+				//alphaTest: 0.1,
 				bumpMap:     THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
 				bumpScale:   0.005,
 				specularMap: THREE.ImageUtils.loadTexture('images/water_4k.png'),
