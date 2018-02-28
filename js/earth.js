c6b37077d97aeace268f065194b63a54b4cbb6ec
Ware, Chris (L&W, Black Mountain) <Chris.Ware@csiro.au>
2018-02-28 14:29:27 +1100

    add click change texture

--- js/earth.js
+++ js/earth.js
@@ -33,16 +33,36 @@
 	light.position.set(5,3,5);
 	scene.add(light);
 	
-    var sphere = createSphere(radius, segments);
+    /* UPDATE THIS
+	var sphere = createSphere(radius, segments);
 	sphere.rotation.y = rotation;
 	//sphere.rotation.x = rotation;
 	//sphere.rotation.y = rotation;
 	scene.add(sphere)
-
+	*/
+		
+	
+	// The textures to use
+	var arr = [
+	 'images/2_no_clouds_4k.jpg',
+	 'images/parC_2016_Global.png',
+	 'images/lu4webgl.png',
+	];
+	var textureToShow = 0;
+	
+	var texture = arr[textureToShow];
+	textureToShow++;
+	
+	var sphere = createSphere(radius, segments, texture);
+	sphere.rotation.y = rotation;
+	//sphere.rotation.x = rotation;
+	//sphere.rotation.y = rotation;
+	scene.add(sphere);
+	
     var clouds = createClouds(radius, segments);
 	//clouds.rotation.x = rotation;
 	clouds.rotation.y = rotation;
-	scene.add(clouds)
+	scene.add(clouds);
 
 	var stars = createStars(90, 64);
 	scene.add(stars);
@@ -63,13 +83,14 @@
 		renderer.render(scene, camera);
 	}
 
+	/* REPLACE THIS WITH A TEXTURE ARG
 	function createSphere(radius, segments) {
 		return new THREE.Mesh(
 			new THREE.SphereGeometry(radius, segments, segments),
 			new THREE.MeshPhongMaterial({
-				//map:         THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg'),
+				map:         THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg'),
 				//map:         THREE.ImageUtils.loadTexture('images/parC_2016_Global.png'),
-				map:         THREE.ImageUtils.loadTexture('images/lu4webgl.png'),
+				//map:         THREE.ImageUtils.loadTexture('images/lu4webgl.png'),
 				// new line below
 				//alphaTest: 0.1,
 				bumpMap:     THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
@@ -79,7 +100,26 @@
 			})
 		);
 	}
-
+	*/
+	
+	function createSphere(radius, segments, texture) {
+		return new THREE.Mesh(
+			new THREE.SphereGeometry(radius, segments, segments),
+			new THREE.MeshPhongMaterial({
+				map:         THREE.ImageUtils.loadTexture(texture),
+				//map:         THREE.ImageUtils.loadTexture('images/parC_2016_Global.png'),
+				//map:         THREE.ImageUtils.loadTexture('images/lu4webgl.png'),
+				// new line below
+				//alphaTest: 0.1,
+				bumpMap:     THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
+				bumpScale:   0.005,
+				specularMap: THREE.ImageUtils.loadTexture('images/water_4k.png'),
+				specular:    new THREE.Color('grey')								
+			})
+		);
+	}
+	
+	
 	function createClouds(radius, segments) {
 		return new THREE.Mesh(
 			new THREE.SphereGeometry(radius + 0.003, segments, segments),			
@@ -99,5 +139,57 @@
 			})
 		);
 	}
+	
+	
+	// ADD CHANGE TEXTURE ON CLICK?
 
+	/*
+	// instantiate a texture loader
+	var loader = new THREE.TextureLoader();
+	//allow cross origin loading
+	loader.crossOrigin = '';
+	*/
+
+	// Load the first texture
+	
+	/* DONT THINK I NEED THIS
+	// Instantiate the material we will be using
+	var material = new THREE.MeshBasicMaterial();
+	// Instantiate a geometry to use
+	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	// Instatiate the mesh with the geometry and material
+	var cube = new THREE.Mesh( geometry, material );
+	cube.position.y = 0.5;
+	*/
+	
+	/* MODIFY AND MOVE THIS ABOVE
+	// Then load the texture
+	loader.load(arr[textureToShow], function(tex) {
+	 // Once the texture has loaded
+	 // Asign it to the material
+	 material.map = tex;
+	 // Update the next texture to show
+	 textureToShow++;
+	 // Add the mesh into the scene
+	 scene.add( cube );
+	});
+	*/
+	
+	// Click interaction
+	var canvas = document.getElementsByTagName("canvas")[0];
+
+	canvas.addEventListener("click", function() {
+	  
+	  //loader.load(arr[textureToShow], function(tex) {
+	  // Once the texture has loaded
+	  // Asign it to the material
+	  //material.map = tex;
+	  // Update the next texture to show
+	  textureToShow++;
+	  // Have we got to the end of the textures array
+	  if(textureToShow > arr.length-1) {
+	   textureToShow = 0;
+	  }
+	 }); 
+	
 }());
