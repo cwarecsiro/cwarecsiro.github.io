f3f6c1d58baddfac8bfc13ac6a0392d33a827fa4
Ware, Chris (L&W, Black Mountain) <Chris.Ware@csiro.au>
2019-02-28 22:27:57 +1100

    back up

--- reca/karma.conf.js
+++
@@ -1,9 +0,0 @@
-//
-// Karma configuration
-//
-// Conditionally load local or CI configurations based on CI environment variable
-//
-
-module.exports = process.env.CI === 'true'
-  ? require( './karma-ci.conf.js' )
-  : require( './karma-local.conf.js' );
